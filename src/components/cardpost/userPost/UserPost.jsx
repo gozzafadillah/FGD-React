baeada4006920760306a90b435e0dccd802425d5
gozzafadillah <gozzafadillah@gmail.com>
2022-12-30 01:43:43 +0700

    fix : fixed display data in detail thread

--- src/components/cardpost/userPost/UserPost.jsx
+++ src/components/cardpost/userPost/UserPost.jsx
@@ -96,7 +96,7 @@ export const UserPost = (props) => {
             </div>
             <Image
               className="image"
-              src="https://images.unsplash.com/photo-1661961110671-77b71b929d52?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+              src={data?.thread?.imageURL}
               preview={false}
               sizes={{ xs: 24, sm: 32, md: 40, lg: 54, xl: 60 }}
             />
